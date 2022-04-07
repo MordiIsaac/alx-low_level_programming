7f819ac5c0fd04a0db6ca099577abd1b59f73e8f
MordiIsaac <imordi488@gmail.com>
2022-04-07 14:43:03 -0700

    kasd

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * Description: main - prints out a string to stdout.
+ *
+ *   * Return: 0 if exited succesfully.
+ *
+ *   */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
